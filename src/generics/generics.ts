f5b0bd1d763fc34f65b9c6132d3e1e719ff5f668
Harpreet S. Randhawa <harpreetwp@gmail.com>
2022-09-28 09:12:11 +0530

    dummy: generic class

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -63,3 +63,42 @@ function sortByKey<T>(data: Array<T>, key: keyof T) {
 
 sortByKey<IFooBar>(fooBars, "foo");
 sortByKey<IFooBar>(fooBars, "bar");
+
+class Animal {
+  public legCount: number;
+  constructor(legCount: number){
+    this.legCount = legCount;
+  }
+}
+
+class Cat extends Animal{
+  constructor() {
+    super(4);
+  }
+}
+
+class Kangaroo extends Animal{
+  constructor() {
+    super(2);
+  }
+}
+
+class Bacteria {
+
+}
+
+function printLegCount<T extends Animal>(animal: T){
+  console.log(`My leg count is: ${animal.legCount}`)
+}
+
+const myCat = new Cat();
+
+const myKangaroo = new Kangaroo();
+
+printLegCount(myCat);
+
+printLegCount(myKangaroo);
+
+const myBacteria = new Bacteria();
+
+printLegCount(myBacteria);
