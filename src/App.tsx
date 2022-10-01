798431a6a325de7df3580b878fc348ba4c9530a4
Harpreet S. Randhawa <harpreetwp@gmail.com>
2022-10-01 10:44:18 +0530

    Crafting a Generic Search Function in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import './App.css';
 import widgets from './mock-data/widgets';
 import persons from './mock-data/persons';
+import genericSearch from './utils/generic-search';
 
 function App() {
+  const query = '';
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>Widgets</h2>
-        {widgets.map((widget) => {
+        {widgets.filter(widget => genericSearch(widget, ['title', 'description'], query, false)).map((widget) => {
           return (
             <h3>{widget.title}</h3>
           )
         })}
 
-        {persons.map((person) => {
+        {persons.filter(person => genericSearch(person, ['firstName', 'lastName', 'eyeColor'], query, false)).map((person) => {
           return (
             <h3>{person.firstName} {person.lastName}</h3>
           )
---
+++ src/utils/generic-search.ts
@@ -0,0 +1,27 @@
+const genericSearch = <T>(
+  object: T,
+  properties: Array<keyof T>,
+  query: string,
+  isCaseSensitive: boolean
+): boolean => {
+
+
+  if(query === ''){
+    return true;
+  }
+
+  return properties.some((property) => {
+    const value = object[property];
+
+    if (typeof value === 'string' || typeof value === 'number') {
+      if(isCaseSensitive)
+        return value.toString().includes(query);
+      else
+        return value.toString().toLocaleLowerCase().includes(query.toLowerCase());
+    }
+
+    return false;
+  });
+};
+
+export default genericSearch;
