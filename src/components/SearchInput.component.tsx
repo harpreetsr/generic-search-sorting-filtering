5c07e0cfd9ada234309448d07c5a660e386e21df
Harpreet S. Randhawa <harpreetwp@gmail.com>
2022-10-01 12:29:12 +0530

    Adding a useDebounce to Reduce Search onChange Event Load

--- src/components/SearchInput.component.tsx
+++ src/components/SearchInput.component.tsx
@@ -1,20 +1,29 @@
+import { useEffect, useState } from "react";
+import { useDebounce } from "../hooks";
 
 interface IProps {
   setSearchQuery: (searchQuery : string) => void;
 }
 
 export const SearchInput = ({setSearchQuery}: IProps) => {
-    return (
-      <>
-        <label htmlFor='search' className='mt-3'>Search! Try me!</label>
-        <input
-         id='search'
-         className='form-control full-width'
-         type="search"
-         placeholder='Search...'
-         aria-label='Search'
-         onChange={(e) => setSearchQuery(e.target.value)}
-        />
-      </>
-    )
+  const [query, setQuery] = useState<string>("");
+  const debouncedQuery = useDebounce(query, 250);
+
+  useEffect(() => {
+    setSearchQuery(debouncedQuery);
+  }, [debouncedQuery, setSearchQuery])
+
+  return (
+    <>
+      <label htmlFor='search' className='mt-3'>Search! Try me!</label>
+      <input
+        id='search'
+        className='form-control full-width'
+        type="search"
+        placeholder='Search...'
+        aria-label='Search'
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </>
+  )
 }
